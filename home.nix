5c5c4c6ed44f44d498097ec2168fe61eb2025f99
Chandler Davis <chandler.davis99@icloud.com>
2024-06-23 22:35:01 +0000

    migrate full helix config

--- home.nix
+++ home.nix
@@ -2,9 +2,7 @@
 {
     home = {
         packages = with pkgs; [
-            hello
             home-manager
-            helix
             gh
         ];
 
@@ -36,6 +34,125 @@
             set -as terminal-features ',rxvt-unicode-256color:clipboard'
             set -sg escape-time 10
           '';
+        };
+
+        helix = {
+            enable = true;
+            defaultEditor = true;
+            settings = {
+                theme = "material_palenight";  
+                editor = {
+                    true-color = true;
+                    auto-format = true;  
+                    indent-guides.render = true;
+
+                    lsp = {
+                        display-inlay-hints = true;
+                    };
+                };
+
+                keys = {
+                    normal = {
+                        "_" = "goto_line_start";
+                        "$" = "goto_line_end";
+                        "{" = "goto_prev_paragraph";  
+                        "}" = "goto_next_paragraph";
+                    };
+
+                    select = {
+                        "_" = "goto_line_start";
+                        "$" = "goto_line_end";
+                        "{" = "goto_prev_paragraph";  
+                        "}" = "goto_next_paragraph";
+                    };
+                };
+            };
+
+            languages = {
+                language = [
+                    {
+                        name = "go";
+                        auto-format = true;
+                        formatter.command = "goimports";
+                    }    
+
+                    {
+                        name = "rust";
+                        auto-format = true;
+                    }
+
+                    {
+                        name = "json";
+                        config = { 
+                            provideFormatter = true; 
+                            json.keeplines.enable = true;
+                        };
+                    }
+
+                    {
+                        name = "typescript";
+                        language-servers = [ "typescript-language-server" "eslint" ];
+                        formatter = {
+                            command = "prettier"; 
+                            args = [ "--parser" "typescript" ];
+                        };
+                        auto-format = true;
+                        indent = {
+                            tab-width = 4;
+                            unit = "    ";
+                        };
+                    }
+
+                    {
+                        name = "javascript";
+                        language-servers = [ "typescript-language-server" "eslint"];
+                        formatter = {
+                            command = "prettier";
+                            args = [ "--parser" "typescript" ];
+                        };
+                        auto-format = true;
+                        indent = {
+                            tab-width = 4;
+                            unit = "    ";
+                        };
+                    }
+
+                    {
+                        name = "json";
+                        formatter = {
+                            command = "prettier";
+                            args = [ "--parser" "json" ];  
+                        };
+                        auto-format = true;
+                        indent = {
+                            tab-width = 4;
+                            unit = "    ";
+                        };
+                    }
+
+                    {
+                        name = "html";
+                        formatter = {
+                            command = "prettier";
+                            args = [ "--parser" "html" ];  
+                        };
+                        indent = {
+                            tab-width = 4;
+                            unit = "    ";
+                        };
+                    }
+
+                    {
+                        name = "svelte";
+                        indent = {
+                            tab-width = 4;
+                            unit = "    ";
+                        };
+                        auto-format = true;
+                        language-servers = [ "svelteserver" "tailwindcss-ls" ];
+                    }
+                ];
+            };
         };
     };
 }
